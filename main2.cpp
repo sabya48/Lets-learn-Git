b3aa596193a2de597b4146df2c204cefb918c631
Sabyasachi Sarkar <kutusarkar9@gmail.com>
2022-11-19 01:05:38 +0530

    added multiply

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 10, b = 10;
+    int sum;
+    sum = a * b;
+    cout << "Hello" << endl;
+    cout << sum << endl;
+    return 0;
+}
